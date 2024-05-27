f05e666431a28d5d303eb8d1ad1d4f9f12d93fb1
hiankarodrigues <hiankarodrigues@creago.org.br>
2024-05-27 10:13:04 -0300

    ajuste problema da porta

--- src/server.ts
+++ src/server.ts
@@ -17,7 +17,8 @@ import { env } from './env'
 app
   .listen({
     port: env.PORT,
+    host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost',
   })
   .then(() => {
-    console.log('HTTP Server Running!')
+    console.log(`HTTP Server Running! On port ${env.PORT}`)
   })
